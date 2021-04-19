c4d4758fd69804d445b35e7a45f44b149e945f0d
danthogi <srivani.danthogi@gmail.com>
2021-04-19 15:38:40 +0530

    first commit

--- Slingshot.js
+++ Slingshot.js
@@ -16,12 +16,12 @@ class SlingShot{
     }
 
     display(){
-        if(this.sling.bodyA){
+       if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+     }
     }
     
 }
